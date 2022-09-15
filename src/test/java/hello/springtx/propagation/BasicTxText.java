58de36c7f0408917f17e841797f4ffb34f3e660f
jmr <jmr10200@gmail.com>
2022-09-15 19:34:52 +0900

    스프링 트랜잭션 전파(propagation)3,4 - 전파 예제

--- src/test/java/hello/springtx/propagation/BasicTxText.java
+++ src/test/java/hello/springtx/propagation/BasicTxText.java
@@ -75,6 +75,23 @@ public class BasicTxText {
         log.info("트랜젝션 2 롤백");
         txManager.rollback(status2);
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜젝션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜젝션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜젝션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜젝션 커밋");
+        txManager.commit(outer);
+
+    }
 }
 /* double_commit() 분석 */
 // 실행결과, 트랜젝션 1 과 트랜젝션 2 가 같은 conn0 커넥션을 사용중이다. connectionPool 때문이다.
@@ -89,3 +106,32 @@ public class BasicTxText {
 /* double_commit_rollback() 분석 */
 // 트랜젝션 1 이 커밋하고, 트랜젝션 2 가 롤백하는 경우, 각각 1 은 커밋, 2 는 롤백된다.
 // 트랜젝션이 각자 관리되므로 각각 동작한다.
+
+/* inner_commit() 분석 */
+// 외부 트랜젝션 수행중, 내부 트랜젝션을 수행한다
+// 외부 트랜젝션은 처음 수행되므로 신규 트랜젝션 이다. (outer.isNewTransaction()=true)
+// 내부 트랜젝션은 이미 외부 트랜젝션이 수행중이므로 false 이다.(inner.isNewTransaction()=false)
+// 즉 내부 트랜젝션이 외부 트랜젝션에 참여 했다. = 외부트랜젝션을 그대로 이어 사용한다.
+// 트랜젝션 참여 : 외부 트랜젝션과 내부 트랜젝션이 하나의 물리 트랜젝션으로 묶이는 것이다.
+
+// 실행결과 : 로그분석
+// 외부 트랜젝션 시작
+// Creating new transaction with name [null]: PROPAGATION_REQUIRED,ISOLATION_DEFAULT
+// Acquired Connection [HikariProxyConnection@209516740 wrapping conn0: url=jdbc:h2:mem:5b615e4a-d952-4c7e-aee8-6f69882a27aa user=SA] for JDBC transaction
+// Switching JDBC Connection [HikariProxyConnection@209516740 wrapping conn0: url=jdbc:h2:mem:5b615e4a-d952-4c7e-aee8-6f69882a27aa user=SA] to manual commit
+//    -> "물리 트랜젝션 시작 (manual commit)"
+// outer.isNewTransaction()=true
+// 내부 트랜젝션 시작
+// Participating in existing transaction -> "존재하는 트랜젝션에 참여한다"
+// inner.isNewTransaction()=false
+// 내부 트랜젝션 커밋
+// 외부 트랜젝션 커밋
+// Initiating transaction commit
+// Committing JDBC transaction on Connection [HikariProxyConnection@209516740 wrapping conn0: url=jdbc:h2:mem:5b615e4a-d952-4c7e-aee8-6f69882a27aa user=SA]
+//     -> "물리 트랜젝션의 커밋"
+// Releasing JDBC Connection [HikariProxyConnection@209516740 wrapping conn0: url=jdbc:h2:mem:5b615e4a-d952-4c7e-aee8-6f69882a27aa user=SA] after transaction
+
+// 정리
+// 스프링은 이렇게 여러 트랜젝션이 함께 사용되는 경우,
+// 처음 트랜젝션을 시작한 외부 트랜젝션이 실제 물리트랜젝션을 관리하도록 한다.
+// 이를 통해 트랜젝션 중복 커밋 문제를 해결한다.
